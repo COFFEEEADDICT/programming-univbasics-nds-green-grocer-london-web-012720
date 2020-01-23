029742e07eabf7adebb8e2fd794845baf00a8b5d
Bryn Heath <brynheath1@gmail.com>
2020-01-23 16:18:44 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -36,7 +36,7 @@ i = 0
     cart_item = find_item_by_name_in_collection(coupons[i][:item], cart)
      coup_item_name = "#{coupons[i][:item]} W/COUPON"
      cart_item_with_coupon = find_item_by_name_in_collection(coup_item_name, cart)
-     if cart_item && cart_item[:count]
+     if cart_item && cart_item[:count] >= coupons[i][:num]
 
     i+ =1
   end
